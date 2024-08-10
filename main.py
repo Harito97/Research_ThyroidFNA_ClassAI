2a797cc7d708eb83361cbd9132fd6644d3433ee3
Harito97 <harito9703@gmail.com>
2024-08-10 08:40:52 +0700

    chore: Add initial project structure and configuration files

---
+++ main.py
@@ -0,0 +1,21 @@
+import argparse
+import yaml
+from src.experiments import * #experiment_a, experiment_b
+
+def main(config_path):
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    
+    if config['experiment'] == 'a':
+        experiment_a.run(config)
+    elif config['experiment'] == 'b':
+        experiment_b.run(config)
+    # Add more experiments
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run medical image classification experiment')
+    parser.add_argument('--config', type=str, required=True, help='Path to config file')
+    args = parser.parse_args()
+    main(args.config)
+
+    # Eg: python main.py --config configs/experiment_a.yaml
