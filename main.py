3c3823fb7b6b60dc35c8b2e3a0bf61b30160b51b
Harito97 <harito9703@gmail.com>
2024-08-13 15:41:35 +0700

    chore: Add code for experiment 43

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import src.experiments.experiment_H97_9_7_EfficientNetB0 as experiment_H97_9_7_E
 import src.experiments.experiment_H97_9_7_EfficientNetB7 as experiment_H97_9_7_EfficientNetB7
 import src.experiments.experiment_H0_EfficientNetB0 as experiment_H0_EfficientNetB0  
 import src.experiments.experiment_H0_EfficientNetB7 as experiment_H0_EfficientNetB7
+import src.experiments.experiment_43 as experiment_43
 ## Import all data modules
 from src.data import data_creator, data_explore
 
@@ -50,6 +51,8 @@ def run_task(config: Dict[str, Any]) -> None:
             experiment_H0_EfficientNetB7.run(config) # use model H0_EfficientNetB7, batch_size = 32
         elif config["experiment"] in ["36", "37", "38", "39", "40", "41"]: # Experiment 36 to 41
             experiment_H97_9_7_EfficientNetB7.run(config) # use model H97_9_7_EfficientNetB7, batch_size = 32 vs 16 if retrains whole network
+        elif config["experiment"] in ["43"]: # Experiment 43
+            experiment_43.run(config)
     else:
         raise ValueError(f"Unknown task type: {task_type}")
 
