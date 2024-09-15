c999dd151aa0f33e364fe6343818e8fe360defda
Harito97 <harito9703@gmail.com>
2024-09-16 00:39:03 +0700

    Refactor model architecture initialization
    - Refactor the `get_cnn_model` function in `cnn.py` to improve code readability and maintainability.
    - Update the model initialization for VGG, ResNet, DenseNet, EfficientNet, and MobileNet architectures.
    - Replace the last fully connected layer with a new linear layer for each model.
    - This change ensures that the models are compatible with the specified number of output classes.
    
    Refactor model architecture initialization
    
    - Refactor the `get_vit_model` function in `vit.py` to improve code readability and maintainability.
    - Update the model initialization for ViT (Vision Transformer) architectures.
    - Replace the head layer with a new linear layer to match the specified number of output classes.

--- phase3_140924/src/models/module1/cnn.py
+++ phase3_140924/src/models/module1/cnn.py
@@ -2,28 +2,51 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
+
 def get_cnn_model(name, num_classes=3):
-    if name == 'vgg16':
-        model = models.vgg16(pretrained=True, num_classes=num_classes)
-    elif name == 'vgg19':
-        model = models.vgg19(pretrained=True, num_classes=num_classes)
-    elif name == 'resnet18':
-        model = models.resnet18(pretrained=True, num_classes=num_classes)
-    elif name == 'resnet152':
-        model = models.resnet152(pretrained=True, num_classes=num_classes)
-    elif name == 'densenet121':
-        model = models.densenet121(pretrained=True, num_classes=num_classes)
-    elif name == 'densenet201':
-        model = models.densenet201(pretrained=True, num_classes=num_classes)
-    elif name == 'efficientnet_b0':
-        model = models.efficientnet_b0(pretrained=True, num_classes=num_classes)
-    elif name == 'efficientnet_b7':
-        model = models.efficientnet_b7(pretrained=True, num_classes=num_classes)
-    elif name == 'mobilenet_v1':
-        model = models.mobilenet_v2(pretrained=True, num_classes=num_classes)
-    elif name == 'mobilenet_v3_large':
-        model = models.mobilenet_v3_large(pretrained=True, num_classes=num_classes)
+    if name == "vgg16":
+        model = models.vgg16(pretrained=True)
+        model.classifier[6] = nn.Linear(in_features=4096, out_features=num_classes)
+    elif name == "vgg19":
+        model = models.vgg19(pretrained=True)
+        model.classifier[6] = nn.Linear(in_features=4096, out_features=num_classes)
+    elif name == "resnet18":
+        model = models.resnet18(pretrained=True)
+        model.fc = nn.Linear(in_features=model.fc.in_features, out_features=num_classes)
+    elif name == "resnet152":
+        model = models.resnet152(pretrained=True)
+        model.fc = nn.Linear(in_features=model.fc.in_features, out_features=num_classes)
+    elif name == "densenet121":
+        model = models.densenet121(pretrained=True)
+        model.classifier = nn.Linear(
+            in_features=model.classifier.in_features, out_features=num_classes
+        )
+    elif name == "densenet201":
+        model = models.densenet201(pretrained=True)
+        model.classifier = nn.Linear(
+            in_features=model.classifier.in_features, out_features=num_classes
+        )
+    elif name == "efficientnet_b0":
+        model = models.efficientnet_b0(pretrained=True)
+        model.classifier[1] = nn.Linear(
+            in_features=model.classifier[1].in_features, out_features=num_classes
+        )
+    elif name == "efficientnet_b7":
+        model = models.efficientnet_b7(pretrained=True)
+        model.classifier[1] = nn.Linear(
+            in_features=model.classifier[1].in_features, out_features=num_classes
+        )
+    elif name == "mobilenet_v1":
+        model = models.mobilenet_v2(pretrained=True)
+        model.classifier[1] = nn.Linear(
+            in_features=model.classifier[1].in_features, out_features=num_classes
+        )
+    elif name == "mobilenet_v3_large":
+        model = models.mobilenet_v3_large(pretrained=True)
+        model.classifier[3] = nn.Linear(
+            in_features=model.classifier[3].in_features, out_features=num_classes
+        )
     else:
         raise ValueError(f"Model name {name} is not recognized.")
-    
+
     return model
--- phase3_140924/src/models/module1/vit.py
+++ phase3_140924/src/models/module1/vit.py
@@ -1,12 +1,20 @@
 import torch
-from torchvision.models import vision_transformer as vit
+import torch.nn as nn
+import timm
+
 
 def get_vit_model(name, num_classes):
-    if name == 'vit_b_16':
-        model = vit.vit_b_16(pretrained=True, num_classes=num_classes)
-    elif name == 'vit_l_16':
-        model = vit.vit_l_16(pretrained=True, num_classes=num_classes)
+    if name == "vit_b_16":
+        model = timm.create_model("vit_base_patch16_224", pretrained=True)
+        model.head = nn.Linear(
+            in_features=model.head.in_features, out_features=num_classes
+        )
+    elif name == "vit_l_16":
+        model = timm.create_model("vit_large_patch16_224", pretrained=True)
+        model.head = nn.Linear(
+            in_features=model.head.in_features, out_features=num_classes
+        )
     else:
         raise ValueError(f"Model name {name} is not recognized.")
-    
+
     return model
